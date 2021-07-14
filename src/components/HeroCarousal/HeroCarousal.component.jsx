2f7dfc932acffc3991af22ba658ae120667b7a75
Pawan kumar <pawanruna54@gmail.com>
2021-07-14 00:51:08 -0700

    add Navbar and carousel

---
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import HeroSlider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const HeroCarousal = () => {
+    const settings = {
+        arrows: true,
+        centerMode: true,
+        centerPadding: "200px",
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+    const images = [
+        "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80,",
+        "https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+        "https://images.unsplash.com/photo-1626074961596-cab914d9392e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80",
+        "https://images.unsplash.com/photo-1626173078406-f83c07e45bc0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    ];
+
+    return (
+        <>
+            <HeroSlider {...settings}>
+                {images.map((image) => (
+                    <div className="w-20 h-80">
+                        <img src={image} alt="testing" className="w-full h-full" />
+                    </div>
+                ))}
+            </HeroSlider>
+        </>
+    );
+};
+
+export default HeroCarousal;
