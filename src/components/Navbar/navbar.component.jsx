1db10e8d8d445a22a631282064b98f1a95e7d8bb
Pawan kumar <pawanruna54@gmail.com>
2021-07-15 01:10:52 -0700

    add hero carousel

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BiChevronRight, BiSearch, BiMenu,BiChevronDown } from "react-icons/bi";
+import { BiChevronRight, BiSearch, BiMenu, BiChevronDown } from "react-icons/bi";
 
 const NavSm = () => {
     return (
@@ -40,16 +40,16 @@ const NavLg = () => {
                             className="w-full h-full"
                         />
                     </div>
-                    <div className="w-full my-2 px-4 py-2 flex items-center gap-2 bg-white rounded-md">
+                    <div className="w-full my-3 px-4 py-2 flex items-center gap-2 bg-white rounded-md">
                         <BiSearch />
                         <input type="search" className="w-full bg-transparent border-none focus:outline-none" placeholder="Search for movies, events, Plays, Sports and Activities." />
                     </div>
                 </div>
                 <div className="flex items-center gap-3">
-                <span className="text-gray-200 text-xs flex items-center cursor-pointer hover:text-white">
-            Dharamshala <BiChevronDown />
-          </span>
-                    <button className="bg-red-600 text-white px-2 py-1 text-sm rounded" > Sign in</button>
+                    <span className="text-gray-200 text-xs flex items-center cursor-pointer hover:text-white">
+                        Dharamshala <BiChevronDown />
+                    </span>
+                    <button className="bg-red-400 text-white px-2 py-1 text-sm rounded" > Sign in</button>
                     <div className="w-8 h-8 text-white">
                         <BiMenu className="w-full h-full" />
                     </div>
